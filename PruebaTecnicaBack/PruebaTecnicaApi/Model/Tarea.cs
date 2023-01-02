eb3b6a69bd99e28ff343f07369472b2d9ad4c631
GustavoAngel <gustavoandradeangel@hotmail.com>
2023-01-02 09:32:35 -0600

    result

---
+++ PruebaTecnicaBack/PruebaTecnicaApi/Model/Tarea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaApi.Model
+{
+    public class Tarea
+    {
+        public Guid Id { get; set; }
+
+        public Guid IdMeta { get; set; }
+        public string Nombre { get; set; }
+
+        public DateTime FechaCreada { get; set; }
+
+        public string Estado { get; set; }
+        public bool IsImportant { get; set; }
+    }
+}
